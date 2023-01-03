083d5015cbc71b04bd82ea8c8a2e2b746959de31
gxy12345 <254768511@qq.com>
2023-01-03 23:57:31 +0800

    fix wrong plugin name

--- model/gsCfg.js
+++ model/gsCfg.js
@@ -7,7 +7,7 @@ import lodash from 'lodash'
 import {
 	isV3
 } from '../components/Changelog.js';
-const plugin = "xiaoyao-cvs-plugin"
+const plugin = "windoge-plugin"
 /**
  * 配置文件
  * 主要用于处理 stoken以及云原神账号数据
