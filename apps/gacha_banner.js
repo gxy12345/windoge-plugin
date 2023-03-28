7606488784fe6de77f2d8a61f4e09e4b27f89670
Windoge <254768511@qq.com>
2023-03-28 14:50:15 +0800

    修改默认卡池来源为github

--- apps/gacha_banner.js
+++ apps/gacha_banner.js
@@ -7,13 +7,13 @@ import { isV3 } from "../components/Changelog.js";
 const _path = process.cwd();
 const res_layout_path = `/plugins/windoge-plugin/resources/common/layout/`;
 const character_banner_data_url = [
-    "https://raw.fastgit.org/gxy12345/windoge-plugin/main/config/banner/character.json",
+    "https://raw.githubusercontent.com/gxy12345/windoge-plugin/main/config/banner/character.json",
     "https://raw.fastgit.org/KeyPJ/genshin-gacha-banners/master/public/data/character.json",
     "https://raw.githubusercontent.com/KeyPJ/genshin-gacha-banners/master/public/data/character.json",
     "https://genshin-gacha-banners.52v6.com/data/character.json",
 ]
 const weapon_banner_data_url = [
-    "https://raw.fastgit.org/gxy12345/windoge-plugin/main/config/banner/weapon.json",
+    "https://raw.githubusercontent.com/gxy12345/windoge-plugin/main/config/banner/weapon.json",
     "https://raw.fastgit.org/KeyPJ/genshin-gacha-banners/master/public/data/weapon.json",
     "https://raw.githubusercontent.com/KeyPJ/genshin-gacha-banners/master/public/data/weapon.json",
     "https://genshin-gacha-banners.52v6.com/data/weapon.json",
